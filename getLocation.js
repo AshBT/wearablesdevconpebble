b4d84294535ade4c30548de678c036eae2c60bf9
AshBt <ashley.beattie@gmail.com>
2014-03-05 11:55:05 -0500

    test number 5

--- getLocation.js
+++ getLocation.js
@@ -3,6 +3,6 @@ navigator.geolocation.getCurrentPosition(function(pos) {
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
       'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: "ashley", subtitle: data.main.temp });
+    simply.text({ title: data.name, subtitle: data.main.temp });
   });
 });
